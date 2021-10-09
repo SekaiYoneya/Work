7eac58d526bc02fadd5b758df740733ccb161292
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-10-09 15:48:39 +0300

    Update botnet.sh

--- botnet.sh
+++ botnet.sh
@@ -1,4 +1,4 @@
-echo "Удаляю кэш..."
+echo ""
 cd /home/Sekai_Yoneya/botnet/ && rm -rf __pycache__
-echo "Запускаю..."
+echo "Кэш был очищен. Запуск..."
 cd /home/Sekai_Yoneya/botnet/ && python3 -B raid.py
