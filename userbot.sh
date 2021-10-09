d99854e9baec0a11dd0b39ccfeaf3fc5f07d69fe
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-10-09 15:53:59 +0300

    Update userbot.sh

--- userbot.sh
+++ userbot.sh
@@ -1,4 +1,3 @@
 echo ""
-cd /home/Sekai_Yoneya/userbot/friendly-telegram/ && rm -rf __pycache__
-echo ">> Кэш был очищен. Запуск..."
+echo ">> Запуск..."
 cd /home/Sekai_Yoneya/userbot/ && python3 -B friendly-telegram
